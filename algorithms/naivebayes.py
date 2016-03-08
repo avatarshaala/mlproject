9ed2d44fe8906f387c43772576d92c84edf73dd3
dipesh <dipesh@dipesh-PC>
2016-03-07 23:01:26 -0600

    Updated

--- algorithms/naivebayes.py
+++ algorithms/naivebayes.py
@@ -13,7 +13,7 @@ def getpriors(targets):
     priors.update((x,y/n) for x, y in priors.items())
 
     return priors
-
+'''features are the notation (such as x, y, z for 3-D feature vector) for feature vector'''
 def getlikelihood(instances, targets):
     likelihoods = {}
     if len(instances) != len(targets):
@@ -21,9 +21,13 @@ def getlikelihood(instances, targets):
         return
     i = 0
     for instance in instances:
-        conditional = instance + " | " + targets[i]
-        if conditional in likelihoods:
-            pass
+        features = instance.split(",")
+        j = 1
+        for feature in features:
+            feature = "x{}={}".format(j,feature)
+            conditional = feature + " | " + "y={}".format(targets[i])
+            if conditional in likelihoods:
+                pass
 
 
 def train(instances, target):
