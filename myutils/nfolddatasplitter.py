2a662675e3d3c865620fa35b177097122b1a6177
gautamdipesh <gautamdipesh@localhost>
2016-03-10 16:22:00 -0600

    Naive Bayes an perceptron updated with code clieanups

--- myutils/nfolddatasplitter.py
+++ myutils/nfolddatasplitter.py
@@ -2,6 +2,9 @@
 __author__ = 'Dipesh Gautam' \
              'email: dgautam@memphis.edu'
 '''---------------------------------------------------'''
+
+from random import shuffle
+
 '''
  56 data points
         10 folds
@@ -20,13 +23,18 @@ __author__ = 'Dipesh Gautam' \
 
 '''
 
-def get_fold_indices(datalength, numoffolds):
+def get_fold_indices(datalength, numoffolds, shuffled=False):
 
+    #datalist = [i for i in range(datalength)]
+
+    allindices = [i for i in range(0,datalength)]
+    if shuffled:
+        shuffle(allindices)
     validationlist = []
     foldlength = int(datalength / numoffolds)
     #each fold gets one more data point until residue is zero
     residue = datalength % numoffolds
-    allindices = set([i for i in range(0,datalength)])
+
     for i in range(0, numoffolds):
         share = 0
         if residue > 0:
@@ -34,15 +42,15 @@ def get_fold_indices(datalength, numoffolds):
             residue -= 1
         testmin = i * (foldlength + share)
         testmax = testmin + (foldlength + share) - 1
-        testindices = [j for j in range(testmin,testmax + 1)]
-        trainindices = sorted(list(allindices-set(testindices)))
+        testindices = [allindices[j] for j in range(testmin,testmax + 1)]
+        trainindices = sorted(list(set(allindices)-set(testindices)))
         validationlist.append((testindices,trainindices))
 
     return validationlist
 
 '''
 #test code below
-lst = get_fold_indices(56, 10)
+lst = get_fold_indices(56, 10, shuffled = True)
 
 print(lst[3])
 '''
