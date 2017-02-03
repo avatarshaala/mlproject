7b62933ff66d3a29052f2c2fa312f63150a698ce
dipeshgautam <dipeshgautam@localhost>
2017-02-03 00:47:08 -0600

    mlp

--- algorithms/mlp1.py
+++ algorithms/mlp1.py
@@ -5,14 +5,16 @@ from random import random
 class mlp1:
     def __init__(self, layers=None):
 
-
-
+        # layers = [5, 7, 9, 3] means 5 input units , 7 units in hidden layer, 9 units in hidden layer and 3 output units
+        self.layers = layers
         self.network = list()
 
-        for i in range(len(layers)):
+
+        for i in range(len(self.layers)):
             layer = None;
             if i >0: #except input layer
-                layer = [{'weights': [random() for i in range(layers[i-1])]} for i in range(layers[i])]
+                layer = [{'weights': [random() for i in range(self.layers[i-1])]} for i in range(self.layers[i])]
+
             self.network.append(layer)
 
         #     hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
@@ -26,3 +28,8 @@ class mlp1:
 
     def initialize(self,layers=None):
         pass
+
+    def train(self):
+
+        for i in range(self.layers):
+
